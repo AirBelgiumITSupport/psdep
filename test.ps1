6655a3938d2a70e91fa9972fa0dd65b9f499ba0e
AirBelgiumITSupport <itsupport@airbelgium.com>
2017-01-11 14:19:39 +0100

    test 2

--- test.ps1
+++ test.ps1
@@ -19,3 +19,173 @@ Write-Host "Here are the top 10 CPU consuming processes right now"
 Write-Host " "
 Get-Process | Sort-Object CPU -Descending | Select-Object -First 10
 
+
+
+#Custom variables
+$wdTypes = Add-Type -AssemblyName 'Microsoft.Office.Interop.Word' -Passthru
+$wdSaveFormat = $wdTypes | Where {$_.Name -eq "wdSaveFormat"}
+$NeedIt = 0
+$SignatureName = 'New Mails Signature'
+$SignatureNameReply = 'Reply Signature'
+$SignatureVer = '1.1'
+$UseSignOnNew = '1'        #If set to '0', the signature will be added as signature for new mails.
+$UseSignOnReply = '1'      #If set to '0', the signature will be added as signature for reply mails.
+$ForceSignatureNew = '1'   #If set to '0', the signature will be editable in Outlook and if set to '1' will be non-editable and forced - forced also as reply.
+$ForceSignatureReply = '1' #If set to '0', the signature will be editable in Outlook and if set to '1' will be non-editable and forced.
+
+
+ 
+#Environment variables
+$AppData=(Get-Item env:appdata).value
+$SigPath = '\Microsoft\Signatures'
+
+$LocalSignaturePath = $AppData+$SigPath
+$RemoteSignaturePathFull = $SigSource
+$UserName = $env:username
+
+
+
+#Retriving implementations of Signatures from registry
+$SignatureInstVer = Get-ItemProperty -Name 'VersionSignature' -Path HKCU:'\Software\Microsoft\Office\16.0\Common\MailSettings' -ErrorAction SilentlyContinue
+
+If (Get-ItemProperty -Name 'VersionSignature' -Path HKCU:'\Software\Microsoft\Office\16.0\Common\MailSettings' -ErrorAction SilentlyContinue)
+{
+    $SignatureInstVer = Get-ItemProperty -Name 'VersionSignature' -Path HKCU:'\Software\Microsoft\Office\16.0\Common\MailSettings' -ErrorAction SilentlyContinue
+    $SignatureInstVer = $SignatureInstVer.VersionSignature.ToString()
+}
+Else { 
+    $SignatureInstVer = '0'
+} 
+
+#Using data to determinate if change of signature is needed
+If ($SignatureVer -ne $SignatureInstVer)
+{
+    $NeedIt = 1
+}
+
+
+#Implementation Of Signature
+
+If ($NeedIt -gt 0)
+{
+    #Check signature path (needs to be created if a signature has never been created for the profile
+    if (!(Test-Path -path $LocalSignaturePath)) {
+           New-Item $LocalSignaturePath -Type Directory
+    }
+    [void][Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic')
+    $strName = [Microsoft.VisualBasic.Interaction]::InputBox("Enter your full name (ex: Albert Einstein)","Enter Full Name")
+    $strTitle = [Microsoft.VisualBasic.Interaction]::InputBox("Enter your Job Title :", "Enter your Job Title")
+    $strPhone = [Microsoft.VisualBasic.Interaction]::InputBox("Enter your mobile phone number (ex: +32 472 11 11 11) :", "Enter mobile Number")
+    $strPhone2 = [Microsoft.VisualBasic.Interaction]::InputBox("Enter your business phone number (ex: +32 10 23 45 XX) :", "Enter Business Phone number")
+
+    
+    $stream = [System.IO.StreamWriter] "$LocalSignaturePath\\New Mails Signature New Year.htm"
+    $stream.WriteLine("<!DOCTYPE HTML PUBLIC `"-//W3C//DTD HTML 4.0 Transitional//EN`">")
+    $stream.WriteLine("<HTML><HEAD><TITLE>Signature</TITLE>")
+    $stream.WriteLine("<META http-equiv=Content-Type content=`"text/html; charset=windows-1252`">")
+    $stream.WriteLine("<BODY>")
+    $stream.WriteLine('<table style="font-family:Arial;border-spacing:0px;color:#000000;font-size:12px; border-collapse:collapse" cellspacing="0" cellpadding="0" border="0"><tr><td style=" padding:0cm 0cm 0cm 0cm;" width="631"><b>')
+    $stream.WriteLine($strName + "</b><br />" + $strTitle)
+    $stream.WriteLine('</td></tr><tr><td style="padding:7pt 5pt 0cm 0cm;"><a href="http://www.airbelgium.com/" target="_blank"><img src="http://airbelgium.com/signatures/signatureLogo.png" id="x_Picture_x0020_53" style="width: 2.5052in; height: 0.7864in;" width="241" border="0" height="76"></a></td></tr><tr><td style="padding:7pt 5pt 0cm 0cm;"><b>M.</b>&nbsp;')
+    $stream.WriteLine($strPhone + '&nbsp;|&nbsp;<b>T.</b>&nbsp;' + $strPhone2)
+    $stream.WriteLine('</td></tr><tr><td style="padding:7pt 5pt 0cm 0cm;"><b><span style="line-height:115%; color:#404040" lang="EN-US">AIR BELGIUM S.A.</span></b><br />Rue Emile Francqui, 7 | 1435 Mont-Saint-Guibert<br />Belgium</td></tr><tr style="padding-bottom: 5cm;"><td style="padding:7pt 5pt 0cm 0cm;">')
+    $stream.WriteLine('<a href="http://www.facebook.com/" target="_blank"><img src="http://airbelgium.com/signatures/signatureFacebook.png" alt="Facebook" style="width: 0.1979in; height: 0.1979in;" width="19" border="0" height="19"></a>&nbsp;<a href="http://www.twitter.com/" target="_blank"><img src="http://airbelgium.com/signatures/signatureTwitter.png" alt="Twitter" style="width: 0.1927in; height: 0.1927in;" width="19" border="0" height="19"></a>&nbsp;<a href="https://www.linkedin.com/company/10647555?trk=tyah&amp;trkInfo=tarId:1472236574956,tas:air%20belgium,idx:2-1-2" target="_blank"><img src="http://airbelgium.com/signatures/signatureLinkedIn.png" alt="LinkedIn" style="width: 0.1927in; height: 0.1927in;" width="19" border="0" height="19"></a>&nbsp;<a href="http://www.youtube.com/" target="_blank"><img src="http://airbelgium.com/signatures/signatureYoutube.png" alt="YouTube" style="width: 0.1927in; height: 0.1927in;" width="19" border="0" height="19"></a>&nbsp;<a href="http://www.airbelgium.com/" target="_blank"><img src="http://airbelgium.com/signatures/signatureWebsite.png" alt="Website" style="width: 0.1927in; height: 0.1927in;" width="19" border="0" height="19"></a></p></td></tr>')
+    $stream.WriteLine('</td></tr><tr><td><img src="http://airbelgium.com/signatures/SignatureNoel.jpg" alt="Merry XMas !" border="0" width="550" height="175" /></td></tr>')
+    $stream.WriteLine('<tr style="height:17.75pt;"><td style="padding:7pt 5pt 0cm 0cm; height:17.75pt" width="631" valign="bottom"><i>This message (including any attachments) contains confidential information intended for a specific individual and purpose, and is protected by law. If you are not the intended recipient, you should delete this message and are hereby notified that any disclosure, duplication, or distribution of this message, or the taking of any action based on it, is strictly prohibited.</i></td></tr></table>')
+    $stream.WriteLine("</BODY>")
+    $stream.WriteLine("</HTML>")
+    $stream.close()
+
+    $stream = [System.IO.StreamWriter] "$LocalSignaturePath\\New Mails Signature.htm"
+    $stream.WriteLine("<!DOCTYPE HTML PUBLIC `"-//W3C//DTD HTML 4.0 Transitional//EN`">")
+    $stream.WriteLine("<HTML><HEAD><TITLE>Signature</TITLE>")
+    $stream.WriteLine("<META http-equiv=Content-Type content=`"text/html; charset=windows-1252`">")
+    $stream.WriteLine("<BODY>")
+    $stream.WriteLine('<table style="font-family:Arial;border-spacing:0px;color:#000000;font-size:12px; border-collapse:collapse" cellspacing="0" cellpadding="0" border="0"><tr><td style=" padding:0cm 0cm 0cm 0cm;" width="631"><b>')
+    $stream.WriteLine($strName + "</b><br />" + $strTitle)
+    $stream.WriteLine('</td></tr><tr><td style="padding:7pt 5pt 0cm 0cm;"><a href="http://www.airbelgium.com/" target="_blank"><img src="http://airbelgium.com/signatures/signatureLogo.png" id="x_Picture_x0020_53" style="width: 2.5052in; height: 0.7864in;" width="241" border="0" height="76"></a></td></tr><tr><td style="padding:7pt 5pt 0cm 0cm;"><b>M.</b>&nbsp;')
+    $stream.WriteLine($strPhone + '&nbsp;|&nbsp;<b>T.</b>&nbsp;' + $strPhone2)
+    $stream.WriteLine('</td></tr><tr><td style="padding:7pt 5pt 0cm 0cm;"><b><span style="line-height:115%; color:#404040" lang="EN-US">AIR BELGIUM S.A.</span></b><br />Rue Emile Francqui, 7 | 1435 Mont-Saint-Guibert<br />Belgium</td></tr><tr style="padding-bottom: 5cm;"><td style="padding:7pt 5pt 0cm 0cm;">')
+    $stream.WriteLine('<a href="http://www.facebook.com/" target="_blank"><img src="http://airbelgium.com/signatures/signatureFacebook.png" alt="Facebook" style="width: 0.1979in; height: 0.1979in;" width="19" border="0" height="19"></a>&nbsp;<a href="http://www.twitter.com/" target="_blank"><img src="http://airbelgium.com/signatures/signatureTwitter.png" alt="Twitter" style="width: 0.1927in; height: 0.1927in;" width="19" border="0" height="19"></a>&nbsp;<a href="https://www.linkedin.com/company/10647555?trk=tyah&amp;trkInfo=tarId:1472236574956,tas:air%20belgium,idx:2-1-2" target="_blank"><img src="http://airbelgium.com/signatures/signatureLinkedIn.png" alt="LinkedIn" style="width: 0.1927in; height: 0.1927in;" width="19" border="0" height="19"></a>&nbsp;<a href="http://www.youtube.com/" target="_blank"><img src="http://airbelgium.com/signatures/signatureYoutube.png" alt="YouTube" style="width: 0.1927in; height: 0.1927in;" width="19" border="0" height="19"></a>&nbsp;<a href="http://www.airbelgium.com/" target="_blank"><img src="http://airbelgium.com/signatures/signatureWebsite.png" alt="Website" style="width: 0.1927in; height: 0.1927in;" width="19" border="0" height="19"></a></p></td></tr>')
+    $stream.WriteLine('<tr style="height:17.75pt;"><td style="padding:7pt 5pt 0cm 0cm; height:17.75pt" width="631" valign="bottom"><i>This message (including any attachments) contains confidential information intended for a specific individual and purpose, and is protected by law. If you are not the intended recipient, you should delete this message and are hereby notified that any disclosure, duplication, or distribution of this message, or the taking of any action based on it, is strictly prohibited.</i></td></tr></table>')
+    $stream.WriteLine("</BODY>")
+    $stream.WriteLine("</HTML>")
+    $stream.close()
+
+    $stream = [System.IO.StreamWriter] "$LocalSignaturePath\\Reply Signature.htm"
+    $stream.WriteLine("<!DOCTYPE HTML PUBLIC `"-//W3C//DTD HTML 4.0 Transitional//EN`">")
+    $stream.WriteLine("<HTML><HEAD><TITLE>Signature</TITLE>")
+    $stream.WriteLine("<META http-equiv=Content-Type content=`"text/html; charset=windows-1252`">")
+    $stream.WriteLine("<BODY>")
+    $stream.WriteLine('<table style="font-family:Arial;border-spacing:0px;color:#000000;font-size:12px; border-collapse:collapse" cellspacing="0" cellpadding="0" border="0"><tr><td style=" padding:0cm 0cm 0cm 0cm;" width="631"><b>')
+    $stream.WriteLine($strName + "</b><br />" + $strTitle)
+    $stream.WriteLine('</td></tr><tr><td style="padding:7pt 5pt 0cm 0cm;"><a href="http://www.airbelgium.com/" target="_blank"><img src="http://airbelgium.com/signatures/signatureLogo.png" id="x_Picture_x0020_53" style="width: 2.5052in; height: 0.7864in;" width="241" border="0" height="76"></a></td></tr>')
+    $stream.WriteLine('<tr style="height:17.75pt;"><td style="padding:7pt 5pt 0cm 0cm; height:17.75pt" width="631" valign="bottom"><i>This message (including any attachments) contains confidential information intended for a specific individual and purpose, and is protected by law. If you are not the intended recipient, you should delete this message and are hereby notified that any disclosure, duplication, or distribution of this message, or the taking of any action based on it, is strictly prohibited.</i></td></tr></table>')
+    $stream.WriteLine("</BODY>")
+    $stream.WriteLine("</HTML>")
+    $stream.close()
+
+
+
+
+
+
+
+
+
+
+
+ 
+    If (Test-Path HKCU:'\Software\Microsoft\Office\16.0')
+ 
+    {
+        $Outlook = 'Outlook'
+        if ($Outlook -ne $null)
+        {
+            Stop-Process -Name $Outlook -Force
+        }
+ 
+        $MSWord = New-Object -comobject word.application
+        $EmailOptions = $MSWord.EmailOptions
+        $EmailSignature = $EmailOptions.EmailSignature
+        $EmailSignatureEntries = $EmailSignature.EmailSignatureEntries
+        If ($UseSignOnNew -eq '1')
+        {
+            $EmailSignature.NewMessageSignature="$SignatureName"
+        }
+        If ($UseSignOnReply -eq '1')
+        {
+            $EmailSignature.ReplyMessageSignature="$SignatureNameReply"
+        }
+        Stop-Process -Name $Outlook
+
+    If ($ForceSignatureNew -eq '1')
+    {
+        If (Get-ItemProperty -Name 'NewSignature' -Path HKCU:'\Software\Microsoft\Office\16.0\Common\MailSettings' -ErrorAction SilentlyContinue) { } 
+        Else { 
+            New-ItemProperty HKCU:'\Software\Microsoft\Office\16.0\Common\MailSettings' -Name 'NewSignature' -Value $SignatureName -PropertyType 'String' -Force 
+            } 
+    }
+
+    If ($ForceSignatureReply -eq '1')
+    {
+        If (Get-ItemProperty -Name 'ReplySignature' -Path HKCU:'\Software\Microsoft\Office\16.0\Common\MailSettings' -ErrorAction SilentlyContinue) { } 
+        Else { 
+            New-ItemProperty HKCU:'\Software\Microsoft\Office\16.0\Common\MailSettings' -Name 'ReplySignature' -Value $SignatureNameReply -PropertyType 'String' -Force
+            } 
+    }
+    }
+
+    #Write Signature specified Registry Values
+    If (Get-ItemProperty -Name 'VersionSignature' -Path HKCU:'\Software\Microsoft\Office\16.0\Common\MailSettings' -ErrorAction SilentlyContinue)
+    {
+        Set-ItemProperty HKCU:'\Software\Microsoft\Office\16.0\Common\MailSettings' -Name 'VersionSignature' -Value $SignatureVer
+    }
+    Else { 
+        New-ItemProperty HKCU:'\Software\Microsoft\Office\16.0\Common\MailSettings' -Name 'VersionSignature' -Value $SignatureVer -PropertyType 'String' 
+    } 
+
+
+    $wshell = New-Object -ComObject Wscript.Shell
+    $wshell.Popup('Script is completed! Mail signatures in Outlook have been updated',0,'All Done',0x0)
+}
