512b43f2f96a9d689cdb7b45024d1979a5c7932a
AirBelgiumITSupport <itsupport@airbelgium.com>
2017-01-16 13:51:38 +0100

    Added Home page and pin application management

--- test.ps1
+++ test.ps1
@@ -87,6 +87,167 @@
         if (Test-Path 'c:\MusicOnHold\Bumper_Tag.wma'){}else{ New-Item 'c:\MusicOnHold' -ItemType Directory }
         Invoke-WebRequest -Uri 'http://airbelgium.com/MusicOnHold/Bumper_Tag.wma' -OutFile 'c:\MusicOnHold\Bumper_Tag.wma'
     }
+	
+
+    #################################################################################################################################
+    #################################################################################################################################
+    #############################################   Update default home Page    #####################################################
+    #########################################  Pin and Upin application to TaskBar  #################################################
+    #################################################################################################################################
+    #################################################################################################################################
+	#New-ItemProperty HKCU:'\Software\AB\ITScript' -Name 'ABScriptVersion' -Value $SpecificUserScriptVersion -PropertyType 'String' -Force 
+	If (Get-ItemProperty -Name 'generalScriptVersion' -Path HKCU:'\Software\AB\ITScript' -ErrorAction SilentlyContinue) { 
+		$scriptInstalledVersion = Get-ItemProperty -Name 'generalScriptVersion' -Path HKCU:'\Software\AB\ITScript'
+		$scriptInstalledVersion = $scriptInstalledVersion.generalScriptVersion.ToString()
+
+		if ($scriptInstalledVersion -eq $generalScriptVersion){
+            $needTOExecuteScript = 0
+        }else{
+			$needTOExecuteScript = 1
+			New-ItemProperty HKCU:'\Software\AB\ITScript' -Name 'generalScriptVersion' -Value $generalScriptVersion -PropertyType 'String' -Force 
+		}
+	} 
+	Else {
+		New-Item -Path HKCU:'\Software\AB\ITScript' -Force 
+		New-ItemProperty HKCU:'\Software\AB\ITScript' -Name 'generalScriptVersion' -Value $generalScriptVersion -PropertyType 'String' -Force 
+		$needTOExecuteScript = 1
+	}
+
+
+	if($needTOExecuteScript -eq 1){	
+
+        function Pin-App ([string]$appname, [switch]$unpin, [switch]$start, [switch]$taskbar, [string]$path) {
+            if ($unpin.IsPresent) {
+                $action = "Unpin"
+            } else {
+                $action = "Pin"
+            }
+    
+            if (-not $taskbar.IsPresent -and -not $start.IsPresent) {
+                Write-Error "Specify -taskbar and/or -start!"
+            }
+    
+            if ($taskbar.IsPresent) {
+                try {
+                    $exec = $false
+                    if ($action -eq "Unpin") {
+                        ((New-Object -Com Shell.Application).NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items() | ?{$_.Name -eq $appname}).Verbs() | ?{$_.Name.replace('&','') -match 'Unpin from taskbar'} | %{$_.DoIt(); $exec = $true}
+                        if ($exec) {
+                            Write "App '$appname' unpinned from Taskbar"
+                        } else {
+                            if (-not $path -eq "") {
+                                Pin-App-by-Path $path -Action $action
+                            } else {
+                                Write "'$appname' not found or 'Unpin from taskbar' not found on item!"
+                            }
+                        }
+                    } else {
+                        ((New-Object -Com Shell.Application).NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items() | ?{$_.Name -eq $appname}).Verbs() | ?{$_.Name.replace('&','') -match 'Pin to taskbar'} | %{$_.DoIt(); $exec = $true}
+                
+                        if ($exec) {
+                            Write "App '$appname' pinned to Taskbar"
+                        } else {
+                            if (-not $path -eq "") {
+                                Pin-App-by-Path $path -Action $action
+                            } else {
+                                Write "'$appname' not found or 'Pin to taskbar' not found on item!"
+                            }
+                        }
+                    }
+                } catch {
+                    Write-Error "Error Pinning/Unpinning $appname to/from taskbar!"
+                }
+            }
+    
+            if ($start.IsPresent) {
+                try {
+                    $exec = $false
+                    if ($action -eq "Unpin") {
+                        ((New-Object -Com Shell.Application).NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items() | ?{$_.Name -eq $appname}).Verbs() | ?{$_.Name.replace('&','') -match 'Unpin from Start'} | %{$_.DoIt(); $exec = $true}
+                
+                        if ($exec) {
+                            Write "App '$appname' unpinned from Start"
+                        } else {
+                            if (-not $path -eq "") {
+                                Pin-App-by-Path $path -Action $action -start
+                            } else {
+                                Write "'$appname' not found or 'Unpin from Start' not found on item!"
+                            }
+                        }
+                    } else {
+                        ((New-Object -Com Shell.Application).NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items() | ?{$_.Name -eq $appname}).Verbs() | ?{$_.Name.replace('&','') -match 'Pin to Start'} | %{$_.DoIt(); $exec = $true}
+                
+                        if ($exec) {
+                            Write "App '$appname' pinned to Start"
+                        } else {
+                            if (-not $path -eq "") {
+                                Pin-App-by-Path $path -Action $action -start
+                            } else {
+                                Write "'$appname' not found or 'Pin to Start' not found on item!"
+                            }
+                        }
+                    }
+                } catch {
+                    Write-Error "Error Pinning/Unpinning $appname to/from Start!"
+                }
+            }
+        }
+
+        function Pin-App-by-Path([string]$Path, [string]$Action, [switch]$start) {
+            if ($Path -eq "") {
+                Write-Error -Message "You need to specify a Path" -ErrorAction Stop
+            }
+            if ($Action -eq "") {
+                Write-Error -Message "You need to specify an action: Pin or Unpin" -ErrorAction Stop
+            }
+            if ((Get-Item -Path $Path -ErrorAction SilentlyContinue) -eq $null){
+                Write-Error -Message "$Path not found" -ErrorAction Stop
+            }
+            $Shell = New-Object -ComObject "Shell.Application"
+            $ItemParent = Split-Path -Path $Path -Parent
+            $ItemLeaf = Split-Path -Path $Path -Leaf
+            $Folder = $Shell.NameSpace($ItemParent)
+            $ItemObject = $Folder.ParseName($ItemLeaf)
+            $Verbs = $ItemObject.Verbs()
+    
+            if ($start.IsPresent) {
+                switch($Action){
+                    "Pin"   {$Verb = $Verbs | Where-Object -Property Name -EQ "&Pin to Start"}
+                    "Unpin" {$Verb = $Verbs | Where-Object -Property Name -EQ "Un&pin from Start"}
+                    default {Write-Error -Message "Invalid action, should be Pin or Unpin" -ErrorAction Stop}
+                }
+            } else {
+                switch($Action){
+                    "Pin"   {$Verb = $Verbs | Where-Object -Property Name -EQ "Pin to Tas&kbar"}
+                    "Unpin" {$Verb = $Verbs | Where-Object -Property Name -EQ "Unpin from Tas&kbar"}
+                    default {Write-Error -Message "Invalid action, should be Pin or Unpin" -ErrorAction Stop}
+                }
+            }
+    
+            if($Verb -eq $null){
+                Write-Error -Message "That action is not currently available on this Path" -ErrorAction Stop
+            } else {
+                $Result = $Verb.DoIt()
+            }
+        }
+
+
+        Set-ItemProperty -Path HKCU:'\Software\Microsoft\Internet Explorer\Main\' -Name 'start page' -Value 'https://airbelgium.sharepoint.com/SitePages/Accueil.aspx' -Force
+        Pin-App "Microsoft Edge" -unpin -taskbar  
+        Pin-App "Microsoft Edge" -unpin -start
+        Pin-App "Skype Preview" -unpin -start
+        Pin-App "Internet Explorer" -pin -taskbar  
+        Pin-App "Internet Explorer" -pin -start                 
+        Pin-App "Outlook 2016" -pin -start                 
+        Pin-App "OneNote 2016" -pin -start   
+        Pin-App "PowerPoint 2016" -pin -start
+        Pin-App "Word 2016" -pin -start   
+        Pin-App "Excel 2016" -pin -start                      
+        Pin-App "Skype for Business 2016" -pin -start   
+        Pin-App "Skype for Business 2016" -pin -taskbar   
+        Pin-App "Store" -unpin -start
+        Pin-App "Store" -unpin -taskbar
+	}
 
 
     #################################################################################################################################
