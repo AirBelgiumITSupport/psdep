00a064bfa47fcd1bf411bb38848cb30bc006a847
AirBelgiumITSupport <itsupport@airbelgium.com>
2017-01-30 13:05:40 +0100

    Removing Christmas signature

--- dynamicScript.ps1
+++ dynamicScript.ps1
@@ -7,7 +7,7 @@
     #Custom variables For Signature Management
     $SignatureName = 'AB New Mails Signature'
     $SignatureNameReply = 'AB Reply Signature'
-    $SignatureVer = '1.6'
+    $SignatureVer = '1.7'
     $UseSignOnNew = '1'        #If set to '0', the signature will be added as signature for new mails.
     $UseSignOnReply = '1'      #If set to '0', the signature will be added as signature for reply mails.
     $ForceSignatureNew = '0'   #If set to '0', the signature will be editable in Outlook and if set to '1' will be non-editable and forced - forced also as reply.
@@ -15,9 +15,7 @@
 
     #Sites to be added as trusted
     $SubDomain = "lync.com", "outlook.com", "microsoftonline.com", "sharepoint.com", "airbusworld.com", "airbusdoc.com", "airbus.com", "airbus2.com"  #This will add: https://*.lync.com/ etc.
-
-
-
+    
 
     #Environnment Variables
     $AppData=(Get-Item env:appdata).value
@@ -25,6 +23,10 @@
     $LocalSignaturePath = $AppData+$SigPath
     $RemoteSignaturePathFull = $SigSource
     $UserName = $env:username
+
+    #Empty microsoft Upload center cache
+    #C:\Users\FabienDelhaye\AppData\Local\Microsoft\Office\Spw    
+    del $AppData'\..\Local\Microsoft\Office\16.0\OfficeFileCache1\*' 2>null
 
 
     #################################################################################################################################
@@ -87,6 +89,7 @@
     #################################################################################################################################
     #############################################   Update default home Page    #####################################################
     #########################################  Pin and Upin application to TaskBar  #################################################
+    ################################################  Adding Trusted Sites  #########################################################
     #################################################################################################################################
     #################################################################################################################################
 	#New-ItemProperty HKCU:'\Software\AB\ITScript' -Name 'ABScriptVersion' -Value $SpecificUserScriptVersion -PropertyType 'String' -Force 
@@ -260,6 +263,60 @@
                     Write-Host "Successfully added '$TruestedSite' domain to trusted Sites in Internet Explorer."
             }
         }
+        
+        #dism.exe /Online /Export-DefaultAppAssociations:C:\AppAssoc.xml
+        #dism.exe /online /Import-DefaultAppAssociations:C:\AppAssoc.xml
+
+        #Set office Document Cache to max 1 day
+        
+        #Set-ItemProperty 'HKCU:\Software\Microsoft\Windows\Shell\Associations\UrlAssociations\ftp\UserChoice' -name ProgId IE.FTP
+        #Set-ItemProperty 'HKCU:\Software\Microsoft\Windows\Shell\Associations\UrlAssociations\http\UserChoice' -name ProgId IE.HTTP
+        #Set-ItemProperty 'HKCU:\Software\Microsoft\Windows\Shell\Associations\UrlAssociations\https\UserChoice' -name ProgId IE.HTTPS
+
+        #Set-ItemProperty 'HKCU:\Software\Microsoft\Windows\Shell\Associations\UrlAssociations\ftp\UserChoice' -name ProgId FirefoxURL
+        #Set-ItemProperty 'HKCU:\Software\Microsoft\Windows\Shell\Associations\UrlAssociations\http\UserChoice' -name ProgId FirefoxURL
+        #Set-ItemProperty 'HKCU:\Software\Microsoft\Windows\Shell\Associations\UrlAssociations\https\UserChoice' -name ProgId FirefoxURL
+
+        <#
+            (Get-ItemProperty 'HKCU:\Software\Microsoft\Windows\Shell\Associations\UrlAssociations\ftp\UserChoice').ProgId
+            (Get-ItemProperty 'HKCU:\Software\Microsoft\Windows\Shell\Associations\UrlAssociations\http\UserChoice').ProgId
+            (Get-ItemProperty 'HKCU:\Software\Microsoft\Windows\Shell\Associations\UrlAssociations\https\UserChoice').ProgId
+        #>
+
+
+
+        <#
+        #powershell
+            $AppsList = "Microsoft.BingFinance","Microsoft.BingNews","Microsoft.BingWeather","Microsoft.XboxApp","Microsoft.MicrosoftSolitaireCollection","Microsoft.BingSports","Microsoft.ZuneMusic","Microsoft.ZuneVideo","Microsoft.Windows.Photos","Microsoft.People","Microsoft.MicrosoftOfficeHub","Microsoft.WindowsMaps","microsoft.windowscommunicationsapps","Microsoft.Getstarted","Microsoft.3DBuilder","Microsoft.Office.Sway"
+
+            ForEach ($App in $AppsList) 
+            { 
+                $PackageFullName = (Get-AppxPackage $App).PackageFullName
+                $ProPackageFullName = (Get-AppxProvisionedPackage -online | where {$_.Displayname -eq $App}).PackageName
+                    write-host $PackageFullName
+                    Write-Host $ProPackageFullName 
+                if ($PackageFullName) 
+                { 
+                    Write-Host "Removing Package: $App"
+                    remove-AppxPackage -package $PackageFullName 
+                } 
+                else 
+                { 
+                    Write-Host "Unable to find package: $App" 
+                } 
+                    if ($ProPackageFullName) 
+                { 
+                    Write-Host "Removing Provisioned Package: $ProPackageFullName"
+                    Remove-AppxProvisionedPackage -online -packagename $ProPackageFullName 
+                } 
+                else 
+                { 
+                    Write-Host "Unable to find provisioned package: $App" 
+                } 
+            }
+
+        #>
+
 
 	 }
 
